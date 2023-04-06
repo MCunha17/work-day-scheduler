10862d37e7f2d2c80f233c9fe13f3a35578e8972
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-04-06 17:42:53 -0500

    Resolved issue that was preventing description to load from the local storage after refresh, by setting an ID for the row element.

--- Develop/script.js
+++ Develop/script.js
@@ -1,82 +1,72 @@
-// Variables for the current date, current hour, and time-blocks
-$(function () {
+$(document).ready(function() {
+  // Variables for the current date, current hour, and time-blocks
   let currentDate = dayjs().format("dddd, MMMM D, YYYY");
   let currentHour = dayjs().format("H");
   let timeBlocks = [];
 
-// Time-blocks for standard business hours
-for (let i = 9; i <= 17; i++) {
-  let timeBlock = {
-    id: `hour-${i}`,
-    hour: i > 12 ? `${i - 12}PM` : `${i}AM`,
-    description: "",
-  };
-  timeBlocks.push(timeBlock);
+  // Time-blocks for standard business hours
+  for (let i = 9; i <= 17; i++) {
+    // Create a time-block object with an id, hour, and description
+    let timeBlock = {
+      id: `hour-${i}`,
+      hour: i > 12 ? `${i - 12}PM` : `${i}AM`,
+      description: "",
+    };
+    timeBlocks.push(timeBlock);
+    
+    // Create a row element for the time-block and add classes based on the current hour
+    let rowEl = $("<div>").addClass("row time-block").attr("id", timeBlock.id);
+    if (i < currentHour) {
+      rowEl.addClass("past");
+    } else if (i > currentHour) {
+      rowEl.addClass("future");
+    } else {
+      rowEl.addClass("present");
+    }
 
-// Row for each time-block
-let rowEl = $("<div>").addClass("row time-block");
-if (i < currentHour) {
-  rowEl.addClass("past");
-} else if (i > currentHour) {
-  rowEl.addClass("future");
-} else {
-  rowEl.addClass("present");
-}
-let hourEl = $("<div>")
-  .addClass("col-2 col-md-1 hour text-center py-3")
-  .text(timeBlock.hour);
-let descriptionEl = $("<textarea>")
-  .addClass("col-8 col-md-10 description")
-  .attr("id", timeBlock.id)
-  .val(timeBlock.description);
-let saveBtn = $("<button>")
-  .addClass("btn saveBtn col-2 col-md-1")
-  .attr("aria-label", "save")
-  .html("<i class='fas fa-save' aria-hidden='true'></i>");
+    // Create a div element for the hour and add classes and text
+    let hourEl = $("<div>")
+      .addClass("col-2 col-md-1 hour text-center py-3")
+      .text(timeBlock.hour);
 
-// Row elements
-  rowEl.append(hourEl, descriptionEl, saveBtn);
-  $(".container-fluid").append(rowEl);
-}
+    // Create a text area element for the event description
+    let descriptionEl = $("<textarea>")
+      .addClass("col-8 col-md-10 description")
+      .attr("id", timeBlock.id)
+      .val(timeBlock.description);
+
+    // Create a save button element
+    let saveBtn = $("<button>")
+      .addClass("btn saveBtn col-2 col-md-1")
+      .attr("aria-label", "save")
+      .html("<i class='fas fa-save' aria-hidden='true'></i>");
   
-// Event listeners for save button
-$(".saveBtn").on("click", function () {
-  let description = $(this).siblings(".description").val();
-  let id = $(this).parent().attr("id");
-  localStorage.setItem(id, description);
-});
-
-// Load saved events from Local Storage
-for (let i = 0; i < timeBlocks.length; i++) {
-  let description = localStorage.getItem(timeBlocks[i].id);
-  if (description !== null) {
-    $(`#${timeBlocks[i].id} .description`).val(description);
+    // Append the hour, description, and save button to the row
+    rowEl.append(hourEl, descriptionEl, saveBtn);
+    // Append the row to the time-block container
+    $("#time-blocks-container").append(rowEl);
   }
-}
 
-// Current date
+  // Event listener for save button click
+  $(".saveBtn").on("click", function () {
+    console.log("Save button clicked!");
+    // Get the event description and id of the time-block
+    let description = $(this).siblings(".description").val();
+    let id = $(this).parent().attr("id");
+    // Store the event description in local storage with the time-block id as the key
+    localStorage.setItem(id, description);
+  });
+
+  // Load saved events from local storage
+  for (let i = 0; i < timeBlocks.length; i++) {
+    // Get the description from local storage for the current time-block
+    let description = localStorage.getItem(timeBlocks[i].id);
+    // If a description exists, set the value of the description text area to the stored value
+    if (description !== null) {
+      $(`#${timeBlocks[i].id} .description`).val(description);
+    }
+  }
+
+  // Set the current date in the header
   $("#currentDay").text(currentDate);
 });
-
-  // Instructions
-  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-  // the code isn't run until the browser has finished rendering all the elements
-  // in the html.
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
