742d162422b2ccf07635e3b6042d5b6481b1877d
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-04-06 23:49:31 -0500

    Updated message so that localStorage displays in red and added green checkmark.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,50 @@
-$(document).ready(function() {
-  // Variables for the current date, current hour, and time-blocks
+$(document).ready(function () {
+  // variables for the current date, current hour, and time blocks
   let currentDay = dayjs().format("dddd, MMMM D, YYYY");
   $("#currentDay").text(currentDay);
   let currentHour = dayjs().format("H");
   let timeBlocks = [];
 
-  // Function to display a message upon saving event
-  function showMessage(message) {
-    // Create a div element for the message
-    let messageEl = $("<div>")
-      .addClass("alert alert-success mt-3")
-      .text(message);
+  // see if message has already been displayed
+  let messageDisplayed = false;
+  const messageEl = $("<div>");
 
-    // Display message at bottom of the header
-    $("header").append(messageEl);
+  // function to display a message after saving event
+  function showMessage(message) {
+    let messageEl = $("<div>").addClass("confirm");
+    let beforeEl = $("<span>").text(message);
+    let storageEl = $("<span>")
+      .addClass("highlight")
+      .html("&nbsp;localStorage &nbsp;");
+    let afterEl = $("<span>").text("");
+    let checkmarkIcon = $("<i>").addClass("fas fa-check-circle green-checkmark");
+
+    // append the message elements to the message div
+    messageEl.append(beforeEl, storageEl, afterEl, checkmarkIcon);
+
+    // display message at bottom of the header
+    if (!messageDisplayed) {
+      $("header").after(messageEl);
+      messageDisplayed = true;
+    }
+
+    // message disappears after 2 seconds
+    setTimeout(() => {
+      messageEl.fadeOut(500, () => messageEl.remove());
+      messageDisplayed = false;
+    }, 2000);
   }
 
-  // Time-blocks for standard business hours
+  // time blocks for standard business hours
   for (let i = 9; i <= 17; i++) {
-    // Create a time-block object with an id, hour, and description
     let timeBlock = {
       id: `hour-${i}`,
       hour: i > 12 ? `${i - 12}PM` : `${i}AM`,
       description: "",
     };
     timeBlocks.push(timeBlock);
-    
-    // Create a row element for the time-block and add classes based on the current hour
+
+    // row element for the time block and add classes based on the current hour
     let rowEl = $("<div>")
       .addClass("row time-block")
       .attr("id", timeBlock.id);
@@ -38,51 +56,47 @@ $(document).ready(function() {
       rowEl.addClass("present");
     }
 
-    // Create a div element for the hour and add classes and text
+    // div element for the hour and add classes and text
     let hourEl = $("<div>")
       .addClass("col-2 col-md-1 hour text-center py-3")
       .text(timeBlock.hour);
 
-    // Create a text area element for the event description
+    // text element for the event description
     let descriptionEl = $("<textarea>")
       .addClass("col-8 col-md-10 description")
       .attr("id", timeBlock.id)
       .val(timeBlock.description);
 
-    // Create a save button element
+    // save button
     let saveBtn = $("<button>")
       .addClass("btn saveBtn col-2 col-md-1")
       .attr("aria-label", "save")
       .html("<i class='fas fa-save' aria-hidden='true'></i>");
-  
-    // Append the hour, description, and save button to the row
+
+    // append the hour, description, and save button to the row
     rowEl.append(hourEl, descriptionEl, saveBtn);
-    // Append the row to the time-block container
+    // append the row to the time block container
     $("#time-blocks-container").append(rowEl);
   }
 
-  // Event listener for save button click
+  // event listener for save button click
   $(".saveBtn").on("click", function () {
-    console.log("Save button clicked!");
-    // Get the event description and id of the time-block
+    // get the event description and id of the time-block
     let description = $(this).siblings(".description").val();
     let id = $(this).parent().attr("id");
-    // Store the event description in local storage with the time-block id as the key
+    // store the event description in local storage with the time-block id as the key
     localStorage.setItem(id, description);
-    // Show message
-    showMessage("Appointment added to the local storage.");
+    // show message
+    showMessage("Appointment Added to");
   });
 
-  // Load saved events from local storage
+  // load saved events from local storage
   for (let i = 0; i < timeBlocks.length; i++) {
-    // Get the description from local storage for the current time-block
+    // get the description from local storage for the current time block
     let description = localStorage.getItem(timeBlocks[i].id);
-    // If a description exists, set the value of the description text area to the stored value
+    // if a description exists, set the value of the description text area to the stored value
     if (description !== null) {
       $(`#${timeBlocks[i].id} .description`).val(description);
     }
   }
-
-  // Set the current date in the header
-  $("#currentDay").text
 });
